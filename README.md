e39f7d56d6d2ff975431affefd17b42711818215
Pim Brouwers <pbrouwers@nhlpa.com>
2022-09-16 08:58:41 -0400

    readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # F# Bricks
 Reusable "bricks" of F# code for scripting and command development.
+
+## `Log.fsx`
+
+Self-contained module for scripting or small console programs.
+
+```fsharp
+Log.info "Hello world!"
+
+Log.fail "I'm meltinggggggg"
+```
--- StringParser.fsx
+++ StringParser.fsx
@@ -1,36 +1,20 @@
 module internal StringParser =
     open System
+    open System.Globalization
 
-    /// Helper to wrap .NET tryParser's.
-    let tryParseWith (tryParseFunc: string -> bool * _) (str : string) =
-        let parsedResult = tryParseFunc str
-        match parsedResult with
+    let parseWith (tryParseFunc: string -> bool * _) =
+        tryParseFunc >> function
         | true, v    -> Some v
         | false, _   -> None
 
-    let parseNonEmptyString x = if StringUtils.strEmpty x then None else Some x
-
-    let parseInt            = tryParseWith Int32.TryParse
-    let parseInt16          = tryParseWith Int16.TryParse
-    let parseInt32          = parseInt
-    let parseInt64          = tryParseWith Int64.TryParse
-    let parseFloat          = tryParseWith Double.TryParse
-    let parseDecimal        = tryParseWith Decimal.TryParse
-    let parseDateTime       = tryParseWith DateTime.TryParse
-    let parseDateTimeOffset = tryParseWith DateTimeOffset.TryParse
-    let parseTimeSpan       = tryParseWith TimeSpan.TryParse
-    let parseGuid           = tryParseWith Guid.TryParse
-
-    /// Attempt to parse boolean from string.
-    ///
-    /// Returns None on failure, Some x on success.
-    ///
-    /// Case-insensitive comparison, and special cases for "yes", "no", "on",
-    /// "off", "1", "0".
-    let parseBoolean (value : string) =
-        let v = value.ToUpperInvariant()
-
-        match v with
-        | "TRUE" | "ON" | "YES" | "1" -> Some true
-        | "FALSE" | "OFF" | "NO" | "0" -> Some false
-        | v -> tryParseWith Boolean.TryParse v
+    let parseInt16          cul = parseWith (fun str -> Int16.TryParse(str, NumberStyles.Currency, cul))
+    let parseInt32          cul = parseWith (fun str -> Int32.TryParse(str, NumberStyles.Currency, cul))
+    let parseInt64          cul = parseWith (fun str -> Int64.TryParse(str, NumberStyles.Currency, cul))
+    let parseInt            cul = parseInt32 cul
+    let parseFloat          cul = parseWith (fun str -> Double.TryParse(str, NumberStyles.Currency, cul))
+    let parseDecimal        cul = parseWith (fun str -> Decimal.TryParse(str, NumberStyles.Currency, cul))
+    let parseDateTime       cul = parseWith (fun str -> DateTime.TryParse(str, cul, DateTimeStyles.AllowWhiteSpaces ||| DateTimeStyles.RoundtripKind))
+    let parseDateTimeOffset cul = parseWith (fun str -> DateTimeOffset.TryParse(str, cul, DateTimeStyles.AllowWhiteSpaces ||| DateTimeStyles.RoundtripKind))
+    let parseTimeSpan       cul = parseWith (fun str-> TimeSpan.TryParse(str, cul))
+    let parseBoolean            = parseWith Boolean.TryParse
+    let parseGuid               = parseWith Guid.TryParse
